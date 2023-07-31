8337cc21f457d76143596bee89c31357ba3fd50c
Anderson Silva <andersonolisilva@gmail.com>
2023-07-30 23:52:32 -0300

    Autenticacao com JWT

--- src/main/java/com/andersonolisilva/config/SecurityConfig.java
+++ src/main/java/com/andersonolisilva/config/SecurityConfig.java
@@ -5,14 +5,19 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.andersonolisilva.security.JWTAuthenticationFilter;
+import com.andersonolisilva.security.JWTUtil;
 
 @EnableWebSecurity
 public class SecurityConfig  extends WebSecurityConfigurerAdapter{
@@ -20,6 +25,11 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
     private static final String[] PUBLIC_MATCHERS = {"/h2-console/**"};
     @Autowired
     private Environment env;
+    @Autowired
+    private JWTUtil jwtUtil;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         if(Arrays.asList(env.getActiveProfiles()).contains("test")){
@@ -27,6 +37,8 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
         }
 
         http.cors().and().csrf().disable();
+
+        http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 
         http.authorizeRequests()
             .antMatchers(PUBLIC_MATCHERS).permitAll()
@@ -44,6 +56,11 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
         return source;
     }
     
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
